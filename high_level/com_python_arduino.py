58959842c4ac683a91381e2e629ce9cd1174b912
DELFORGE Raphaël <raphael.delforge16@gmail.com>
2024-02-01 10:18:08 +0100

    changement de code dans la classe car et autres

--- high_level/com_python_arduino.py
+++ high_level/com_python_arduino.py
@@ -3,17 +3,19 @@ import time
 import struct 
 
 
+
 if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)   #à changer avec le bon port arduino
     ser.reset_input_buffer()
 
-    def sendOrder(ordre : int, argument : int): #octet1 and octet2 should not be more that 8 bits
-        trame = struct.pack("<BB", ordre, argument) 
+    def sendOrder(ordre : int, argument : float): #octet1 and octet2 should not be more that 8 bits
+        trame = struct.pack("<Bf", ordre, argument) 
         ser.write(trame)
 
     while True:
         tmp = input("ordre puis argument : ")
-        ordre, argument = map(int, tmp.split())
+        args = tmp.split()
+        ordre, argument = int(args[0]), float(args[1])
         sendOrder(ordre, argument)
         #print(ser.readline())
         #time.sleep(3)
